2085737a4b8c95c753e6fcbe4c0c49a1ba70692e
Mathie <math039@outlook.fr>
2024-09-14 19:14:45 +0200

    php

--- index.php
+++ index.php
@@ -1,4 +1,18 @@
-<?php
+<!DOCTYPE html>
+<html>
 
-echo "J'ai 92 ans.";
-?>
+<head>
+    <meta charset="utf-8" />
+    <title>Ma page web</title>
+</head>
+
+<body>
+    <h1>Ma page web</h1>
+    <p>
+        Aujourd'hui nous sommes le
+        <?php echo date('d/m/Y h:i:s'); ?>.
+        <?php echo "Cette ligne a été écrite \"uniquement\" en PHP."; ?>
+    </p>
+</body>
+
+</html>
